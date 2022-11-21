e0ded3276f6b3311e46cf5bf97b6b14ad1ebb7dd
Steven Hicks <steven@simula.no>
2022-11-21 01:47:03 +0100

    Add medico evaluation

--- trackeval/datasets/__init__.py
+++ trackeval/datasets/__init__.py
@@ -10,3 +10,4 @@ from .youtube_vis import YouTubeVIS
 from .head_tracking_challenge import HeadTrackingChallenge
 from .rob_mots import RobMOTS
 from .person_path_22 import PersonPath22
+from .medico import Medico
